0563b6fa18248538aba2cd8402c8bd1d4402117c
Sanam Rai <sanamr571@outlook.com>
2026-06-08 20:43:29 +0545

    added logic to chunk the  items with less than 300 characters

--- step1_extract.py
+++ step1_extract.py
@@ -1,10 +1,25 @@
 import pdfplumber
 
+def getChunks(text):
+    chunks = []
+    chunk = text.split(". ")
+    current_chunk = ""
+    for sentence in chunk:
+        if( (len(current_chunk) + len(sentence)) < 300):
+            current_chunk += sentence + ". "
+        else:  
+            chunks.append(current_chunk)
+            current_chunk = sentence + ". "
+    if current_chunk:
+        chunks.append(current_chunk)
+    return chunks
+
 with pdfplumber.open("./pdf.pdf") as pdf:
+    full_text = ""
     for index, page in enumerate(pdf.pages):
-        print(f"page {index + 1}")
         text = page.extract_text()
-        if(text):
-            print(text)
-        else:
+        full_text += text + "\n"
+        if not text:
             print("Warning No  text found")
+    texts = getChunks(full_text)
+    print(texts)
