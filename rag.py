b8da9215eaf2eecfa6eb2485cbbc7614c2e1e257
Sanam Rai <sanamr571@outlook.com>
2026-06-09 11:31:07 +0545

    integrate chromadb and query excution of chunks of text

--- rag.py
+++ rag.py
@@ -1,6 +1,9 @@
 import pdfplumber
 from sentence_transformers import SentenceTransformer
+import chromadb
 
+client = chromadb.PersistentClient(path="./my_chorma_db")
+collection = client.get_or_create_collection(name="knowledge_base")
 def getChunks(text):
     chunks = []
     chunk = text.split(". ")
@@ -32,4 +35,24 @@ embeddings = model.encode(texts)
 print(f"Number of chunks: {len(texts)}")
 print(f"Embeddings shape: {embeddings.shape}")
 print(f"First embedding (first 5 numbers): {embeddings[0][:5]}")
-print(embeddings)
+
+ids = [f"id_{i}" for i in range(len(texts))]
+
+if collection.count() == 0:
+    collection.add(
+        embeddings=embeddings.tolist(),
+        documents=texts,
+        ids=ids
+    )
+    print(f"Stored {collection.count()} chunks")
+else:
+    print(f"Already stored {collection.count()} chunks, skipping")
+query_text = "sanam rai"
+query_embedding = model.encode(query_text).tolist()
+
+results = collection.query(
+    query_embeddings=[query_embedding],
+    n_results=2
+)
+
+print(results['documents'])
